a746b22c79696ea2a4ac5b8bcfd6dbe449b0a778
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-06 14:56:49 +0200

    tried to add ResetBoard, failed

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -15,6 +15,7 @@ namespace MineSweeperLogic
             SizeX = sizeX;
             SizeY = sizeY;
             NumberOfMines = nrOfMines;
+            Positions = new PositionInfo[SizeY,SizeX];
         }
 
         public int PosX { get; private set; }
@@ -23,6 +24,7 @@ namespace MineSweeperLogic
         public int SizeY { get; }
         public int NumberOfMines { get; }
         public GameState State { get; private set; }
+        public PositionInfo[,] Positions { get; private set; }
 
         public PositionInfo GetCoordinate(int x, int y)
         {
@@ -39,6 +41,34 @@ namespace MineSweeperLogic
 
         public void ResetBoard()
         {
+            // Creates epmty positions
+            for (int y = 0; y < Positions.GetLength(0); y++)
+            {
+                for (int x = 0; x < Positions.GetLength(1); x++)
+                {
+                    Positions[y, x].Y = y;
+                    Positions[y, x].X = x;
+                    Positions[y, x].HasMine = false;
+                    Positions[y, x].IsOpen = false;
+                    Positions[y, x].IsFlagged = false;
+                }
+            }
+
+            // Add mines in random positions
+            int currentMines = 0;
+            ServiceBus sBus = new ServiceBus();
+
+            while (currentMines < NumberOfMines)
+            {
+                int randX = sBus.Next(SizeX);
+                int randY = sBus.Next(SizeY);
+
+                if (Positions[randY, randX].HasMine == false)
+                {
+                    Positions[randY, randX].HasMine = true;
+                    currentMines++;
+                }
+            }
         } 
         
         public void DrawBoard()
