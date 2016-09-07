77eb61b8924cc4c4a32bac1fe5fd8ea353f3125f
oscarmolin <oscar.molin@lund.ntig.se>
2016-09-07 10:04:40 +0200

    Added DrawBoard

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -14,19 +14,22 @@ namespace MineSweeperLogic
         {
             SizeX = sizeX;
             SizeY = sizeY;
-            NumberOfMines = NumberOfMines;
+            NumberOfMines = nrOfMines;
+            iSB = bus;
         }
-
+        private IServiceBus iSB ;
+       
         public int PosX { get; private set; }
         public int PosY { get; private set; }
         public int SizeX { get; }
         public int SizeY { get; }
         public int NumberOfMines { get; }
         public GameState State { get; private set; }
+        private string symbol;
 
         public PositionInfo GetCoordinate(int x, int y)
         {
-            return null;
+            return new PositionInfo();
         }
 
         public void FlagCoordinate()
@@ -43,6 +46,77 @@ namespace MineSweeperLogic
 
         public void DrawBoard()
         {
+            for (int i = 0; i < SizeY; i++)
+            {
+                for (int j = 0; j < SizeX; j++)
+                {
+
+                    //color = ConsoleColor.DarkCyan;
+
+                    //if (j == PosX && i == PosY)
+                    //    color = ConsoleColor.DarkCyan;
+                    //else
+                    //    color = ConsoleColor.Black;
+
+                    //if (GetCoordinate(j, i).IsFlagged)
+                    //    symbol = "! ";
+                    //else if (GetCoordinate(j, i).IsOpen)
+                    //{
+                    //    if (GetCoordinate(j, i).HasMine)
+                    //        symbol = "X ";
+                    //    else if (GetCoordinate(j, i).NrOfNeighbours == 0)
+                    //        symbol = ".  ";
+                    //    else
+                    //        symbol = GetCoordinate(j, i).NrOfNeighbours + " ";
+                    //}
+                    //else
+                    //    symbol = "? ";
+                    //iSB.Write(symbol,color);
+
+
+                    if (j == PosX && i == PosY)
+                    {
+                        if (GetCoordinate(j, i).IsFlagged)
+                            symbol = "! ";
+                        else if (GetCoordinate(j, i).IsOpen)
+                        {
+                            if (GetCoordinate(j, i).HasMine)
+                                symbol = "X ";
+                            else if (GetCoordinate(j, i).NrOfNeighbours == 0)
+                                symbol = ".  ";
+                            else
+                                symbol = GetCoordinate(j, i).NrOfNeighbours + " ";
+                        }
+                        else
+                            symbol = "? ";
+                        iSB.Write(symbol,ConsoleColor.DarkCyan);
+                    }
+
+                    else
+                    {
+                        if (GetCoordinate(j, i).IsFlagged)
+                            symbol = "! ";
+                        else if (GetCoordinate(j, i).IsOpen)
+                        {
+                            if (GetCoordinate(j, i).HasMine)
+                                symbol = "X ";
+                            else if (GetCoordinate(j, i).NrOfNeighbours == 0)
+                                symbol = ".  ";
+                            else
+                                symbol = GetCoordinate(j, i).NrOfNeighbours + " ";
+                        }
+                        else
+                            symbol = "? ";
+                        iSB.Write(symbol);
+                    }
+
+
+                  
+
+                }
+                iSB.WriteLine();
+            }
+            
         }
 
         #region MoveCursor Methods
